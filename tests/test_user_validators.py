4d6d879a0bd5410dbc045008a73813a2f295f645
mariah <mariah.kakande@gmail.com>
2019-09-23 12:12:34 +0300

    chore(travis): set up travisCI
    - install nose
    - add a travis.yml file
    
    [Delivers #168692066]

--- tests/test_user_validators.py
+++ tests/test_user_validators.py
@@ -32,3 +32,51 @@ class TestUserValidator(unittest.TestCase):
         }
         self.assertEqual(self.validate.validate_user_fields(data),
                          "username cannot be blank")
+
+    def test_return_if_wrong_datatype(self):
+        # Tests the return value if a wrong value is entered
+        data = {
+            "password": "rtrkhWE1",
+            "username": "maria",
+            "email": "mirembe3@mail.com",
+            "role": 5,
+            "confirm_password": "rtrkhWE1"
+        }
+        self.assertEqual(self.validate.validate_user_fields(data),
+                         "Enter string value at role")
+
+    def test_raises_exception(self):
+        # tests an exception is raised if invalid fields are entered
+        data = {
+            "password": "rtrkhWE1",
+            "username": "maria",
+            "email": "mirembe3@mail.com",
+            "role": "Admin",
+            "confirmassword": "rtrkhWE1"
+        }
+        self.assertEqual(self.validate.validate_user_fields(data),
+                         "Invalid Key added")
+    
+    def test_validate_email_regexes(self):
+        # Tests an error is raised if wrong format data is entered
+        data = {
+            "password": "rtrkhWE1",
+            "username": "maria",
+            "email": "mirembe3mail.com",
+            "role": "Admin",
+            "confirm_password": "rtrkhWE1"
+        }
+        self.assertEqual(self.validate.validate_user_regexes(data),
+                         "Invalid email format")
+    
+    def test_validate_unsimilar_passwords(self):
+        # Tests an error is raised if a wrong password is provided
+        data = {
+            "password": "rtr",
+            "username": "maria",
+            "email": "mirembe3@mail.com",
+            "role": "Admin",
+            "confirm_password": "rtr#DSDSDSS"
+        }
+        self.assertEqual(self.validate.validate_user_password(data),
+                         "passwords dont match")
